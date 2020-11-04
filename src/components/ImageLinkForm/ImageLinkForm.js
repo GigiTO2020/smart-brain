08418b066c633bd531cfd1bca521eadb37acf77a
GigiTO2020 <66763292+GigiTO2020@users.noreply.github.com>
2020-11-03 19:22:33 -0500

    2.0

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'Upload a picture and let Gen the AI detect the face for you'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
